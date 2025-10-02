53885531750c6b3b2d9c67ae7490c978bb266894
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-10-02 11:58:38 +0530

    form with state management

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Login from "./Login";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-export default App
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  return (
+    <div id="main">
+      <h1>Parent Component</h1>
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <Login isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+      )}
+    </div>
+  );
+};
+
+export default App;
